789c4d7ac393de42a1c662627d0b8f69a220a03f
Stas <dark.knight22832@gmail.com>
2023-09-18 12:25:03 +0300

    Select location items list is created

---
+++ lib/presentation/pages/select_location_page/components/location_list_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:simple_weather/assets/colors/app_colors.dart';
+import 'package:simple_weather/data/models/location_dto.dart';
+
+class LocationListItem extends StatelessWidget {
+  final LocationDto location;
+  final Function(LocationDto location) onItemClicked;
+
+  const LocationListItem(
+      {super.key, required this.location, required this.onItemClicked});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Card(
+        surfaceTintColor: Colors.white,
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(location.name, textAlign: TextAlign.start,),
+                    SizedBox(height: 8,),
+                    Text(location.fullName)
+                  ],
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                onItemClicked(location);
+              },
+              icon: Icon(
+                Icons.check_circle_outline_sharp,
+                color: AppColors.primaryAccent,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
